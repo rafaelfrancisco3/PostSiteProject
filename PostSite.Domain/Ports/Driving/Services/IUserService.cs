60d8872f89759d643586ca3a52c0a719f907f137
Rafael <franciscorafael814@gmail.com>
2022-01-14 07:13:54 -0400

    IUserService was created

---
+++ PostSite.Domain/Ports/Driving/Services/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using PostSite.Domain.Entities;
+using PostSite.Domain.Utils;
+
+namespace PostSite.Domain.Ports.Driving.Services
+{
+    public interface IUserService
+    {
+        public Task<Response<IUser>> Authentication(string email, string password);
+        public Task<Response<IUser>> CreateAsync(IUser user, string password);
+    }
+}
