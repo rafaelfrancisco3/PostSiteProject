4b0b88783ca7f1ac3e0b4b4e18f11679bc53f342
Rafael <franciscorafael814@gmail.com>
2022-01-14 09:22:06 -0400

    Dependency module added

---
+++ PostSite.Application/ServiceModuleDependency.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PostSite.Application.Services;
+using PostSite.Domain.Ports.Driving.Services;
+
+namespace PostSite.Application
+{
+    public static class ServiceModuleDependency
+    {
+        public static void AddPostSiteApplication(this IServiceCollection services)
+        {
+            services.AddScoped<IUserService, UserService>();
+        }
+    }
+}
---
+++ PostSite.Infra/InfraModuleDependency.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PostSite.Infra.Data;
+using PostSite.Domain.Ports.Driven;
+using PostSite.Infra.Adapters;
+using PostSite.Domain.Ports.Driven.Repositories;
+using PostSite.Infra.Data.Repositories;
+
+namespace PostSite.Infra
+{
+    public static class InfraModuleDependency
+    {
+        public static void AddPostSiteInfra(this IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>();
+            services.AddTransient<IHashAdapter, HashAdapter>();
+            services.AddScoped<IUserRepository, UserRepository>();
+        }
+    }
+}
