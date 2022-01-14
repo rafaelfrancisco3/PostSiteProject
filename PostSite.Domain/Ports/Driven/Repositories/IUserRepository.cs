a8088a35e25a31fbdb692e651c47c06ce2a647e6
Rafael <franciscorafael814@gmail.com>
2022-01-14 07:13:33 -0400

    IUserRepository was created

---
+++ PostSite.Domain/Ports/Driven/Repositories/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using PostSite.Domain.Entities;
+using PostSite.Domain.Utils;
+
+namespace PostSite.Domain.Ports.Driven.Repositories
+{
+    public interface IUserRepository
+    {
+        public Task<Response<IUser>> CreateAsync(IUser user);
+        public Task<Response<IUser>> GetAsync(string email);
+    }
+}
