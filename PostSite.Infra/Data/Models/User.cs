170a67b80f6fa0b3a65f79a5c83356d8d2ecbae6
Rafael <franciscorafael814@gmail.com>
2022-01-14 08:56:09 -0400

    User model was created

---
+++ PostSite.Infra/Data/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using PostSite.Domain.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace PostSite.Infra.Data.Models
+{
+    public class User : IUser
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string PasswordHash { get; set; }
+    }
+}
