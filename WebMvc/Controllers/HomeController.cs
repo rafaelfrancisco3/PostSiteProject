c91baf5546e0d6226f88fe87b74b7610c80d7708
Rafael <franciscorafael814@gmail.com>
2022-01-14 09:21:11 -0400

    WebMvcAdded

---
+++ WebMvc/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PostSite.Domain.Ports.Driving.Services;
+using PostSite.Infra.Data.Models;
+using System.Diagnostics;
+using WebMvc.Models;
+
+namespace WebMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IUserService _userService;
+
+        public HomeController(ILogger<HomeController> logger, IUserService userService)
+        {
+            _logger = logger;
+            _userService = userService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            await _userService.CreateAsync(new User() { Email = "Rafaelfranc01@gmail.com", UserName = "Rafael"}, "12345678");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
