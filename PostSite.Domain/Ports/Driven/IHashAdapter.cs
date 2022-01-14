1f01f4da8f240c55cb2c902e5b0318e1dc4971ee
Rafael <franciscorafael814@gmail.com>
2022-01-14 07:46:56 -0400

    IHashAdapter was created

---
+++ PostSite.Domain/Ports/Driven/IHashAdapter.cs
@@ -0,0 +1,8 @@
+﻿namespace PostSite.Domain.Ports.Driven
+{
+    public interface IHashAdapter
+    {
+        public string Hash(string password);
+        public bool verify(string password, string passwordHash);
+    }
+}
