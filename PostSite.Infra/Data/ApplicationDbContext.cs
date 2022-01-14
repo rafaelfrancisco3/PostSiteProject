87a95faef4a0bb0382f6fbd03272eb103c44bbe0
Rafael <franciscorafael814@gmail.com>
2022-01-14 08:57:15 -0400

    ApplicationDbContext was created

---
+++ PostSite.Infra/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PostSite.Infra.Data.Models;
+
+namespace PostSite.Infra.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=PostSiteProject;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
