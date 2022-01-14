b4c7414690bcf7e36f7317784fe0df4196e9b14b
Rafael <franciscorafael814@gmail.com>
2022-01-14 07:48:21 -0400

    HashAdapter was created

---
+++ PostSite.Infra/Adapters/HashAdapter.cs
@@ -0,0 +1,17 @@
+﻿using PostSite.Domain.Ports.Driven;
+
+namespace PostSite.Infra.Adapters
+{
+    internal class HashAdapter : IHashAdapter
+    {
+        public string Hash(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public bool verify(string password, string passwordHash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, passwordHash);
+        }
+    }
+}
