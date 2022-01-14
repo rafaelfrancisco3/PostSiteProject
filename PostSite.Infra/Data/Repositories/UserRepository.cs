e0288fdb783be5e2ad6c7cedb8056eb7af903c31
Rafael <franciscorafael814@gmail.com>
2022-01-14 08:56:28 -0400

    UserRepository was created

---
+++ PostSite.Infra/Data/Repositories/UserRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PostSite.Domain.Entities;
+using PostSite.Domain.Ports.Driven.Repositories;
+using PostSite.Domain.Utils;
+using PostSite.Infra.Data.Models;
+
+namespace PostSite.Infra.Data.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Response<IUser>> CreateAsync(IUser user)
+        {
+            Response<IUser> response = new Response<IUser>();
+            await _context.Users.AddAsync((User)user);
+            await _context.SaveChangesAsync();
+            response.Data = user;
+            return response;
+        }
+
+        public async Task<Response<IUser>> GetAsync(string email)
+        {
+            Response<IUser> response = new Response<IUser>();
+            response.Data = await _context.Users.FirstAsync(x => x.Email == email);
+            return response;
+        }
+    }
+}
